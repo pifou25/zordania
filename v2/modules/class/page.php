543c96a72a35a45e1e55f58317fef108bd9fc6ec
Zor90 <34856483+Zor90@users.noreply.github.com>
2020-01-07 16:46:43 +0100

    Update page.php

--- v2/modules/class/page.php
+++ v2/modules/class/page.php
@@ -41,13 +41,14 @@ if($_user['alaid']) {
 	$dpl_atq = new diplo(array('aid' => $_user['alaid']));
 	$dpl_atq_arr = $dpl_atq->actuels(); // les pactes actifs en tableau
 	$_tpl->set('mbr_dpl',$dpl_atq_arr);
-	/*mon état dans l'alliance*/
+	/*mon état dans l'alliance*/	
 	$aetat = get_aetat_mid($_user['mid']);
 	$ambr_aetat= $aetat[0]['ambr_etat'];
 }
-else
+else {
 	$dpl_atq_arr = array();
-
+	$ambr_aetat = 0;
+}
 if($type != 5 && $type != 6)
 	$tab_class = can_atq_lite($tab_class, $_user['pts_arm'], $_user['mid'], $_user['groupe'], $_user['alaid'], $dpl_atq_arr,$ambr_aetat);
 	
